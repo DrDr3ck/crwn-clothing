fcf4e3d4ed84c112f6a5e17aaf75c775a769d1fd
Stephane <stephane.conreaux@yahoo.fr>
2021-05-31 07:30:53 +0200

    sign-in

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -6,7 +6,7 @@ const CollectionItem = ({name, price, imageUrl}) => {
     console.log(imageUrl);
     return(
     <div className='collection-item'>
-        <div className='image' style={{backgroundImage: `url(${imageUrl})`}}></div>
+        <div className='image' style={{backgroundImage: `url(${imageUrl})`, backgroundColor: "lightblue"}}></div>
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
