23589b847a610c1d29c36531dfd3d19624832eea
Stephane <stephane.conreaux@yahoo.fr>
2021-06-08 07:56:29 +0200

    reducer part 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import './index.css';
 import App from './App';
+import store from './redux/store';
 
 ReactDOM.render(
-	<React.StrictMode>
+	<ReduxProvider store={store}>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
-	</React.StrictMode>,
+	</ReduxProvider>,
 	document.getElementById('root')
 );
